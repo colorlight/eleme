534642a2273ab122b68a390016d4d9f2ccf3fd5d
colorlight <colorlightleecaa@gmail.com>
2017-07-17 15:54:15 +0800

    完成时间 模板化输出

--- src/common/js/date.js
+++ src/common/js/date.js
@@ -1,23 +1,25 @@
 export function formatDate(date, fmt) {
-	if (/(y+)/.test(fmt)) {
-		fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
-	}
-	let o = {
-		'M+': date.getMonth() + 1,
-		'd+': date.getDate(),
-		'h+': date.getHours(),
-		'm+': date.getMinutes(),
-		's+': date.getSeconds()
-	};
-	for (let k in o) {
-		if (new RegExp(`(${k})`).test(fmt)) {
-			let str = o[k] + '';
-			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
-		}
-	}
-	return fmt;
+  var tempMatch = fmt.match(/y+/);
+  if (tempMatch) {
+    fmt = fmt.replace(tempMatch[0], (date.getFullYear() + '').substr(4 - tempMatch[0].length));
+  }
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  };
+  for (let k in o) {
+    tempMatch = fmt.match(new RegExp(k));
+    if (tempMatch) {
+      let str = o[k] + '';
+      fmt = fmt.replace(tempMatch[0], (tempMatch[0].length === 1) ? str : padLeftZero(str));
+    }
+  }
+  return fmt;
 };
 
 function padLeftZero(str) {
-	return ('00' + str).substr(str.length);
+  return ('00' + str).substr(str.length);
 }
